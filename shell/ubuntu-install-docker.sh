556828f09a83dcd64116c9b90f5b2d4e22e123bd
Chao <362229416@qq.com>
2018-12-19 14:23:57 +0800

    Update ubuntu-install-docker.sh
    install compose

--- shell/ubuntu-install-docker.sh
+++ shell/ubuntu-install-docker.sh
@@ -10,3 +10,6 @@ sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/li
 sudo apt-get -y update
 sudo apt-get -y install docker-ce
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
